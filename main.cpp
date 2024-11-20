4aa9e4b557370f3d2041d7c40126277548799e53
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-20 20:41:39 +0200

    Completed Task 2

--- main.cpp
+++ main.cpp
@@ -7,6 +7,22 @@ using std::cin;
 
 int main()
 {
+	long long x;
+	long long y;
+	long long result = 1;
+	long long i = 0;
+
+	cout << "Enter the first number: ";
+	cin >> x;
+	cout << "Enter the second number: ";
+	cin >> y;
+	
+	for (; i < y; i++)
+	{
+		result *= x;
+	}
+
+	cout << result << endl;
 
 	_getch();
 	return 0;
