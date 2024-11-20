063ef50668689036eff18b03b2c11c53958b11cd
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-20 20:47:36 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -7,6 +7,17 @@ using std::cin;
 
 int main()
 {
+	int a = 1;
+	int b = 1000;
+	double result = 0;
+
+	while (a <= b)
+	{
+		result += a;
+		a++;
+	}
+
+	cout << "Result: " << result / 1000 << endl;
 
 	_getch();
 	return 0;
