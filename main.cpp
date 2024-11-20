a602a3d10304538098dcfda3ba07b21afe7c1020
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-20 20:09:15 +0200

    Completed Task 1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,24 @@ using std::cin;
 
 int main()
 {
+	int a;
+	long long result = 0;
+
+	cout << "Enter the number: ";
+	cin >> a;
+
+	if (a == 0)
+	{
+		cout << "Result: 0" << endl;
+		return 0;
+	}
+
+	for (; a <= 500; a++)
+	{
+		result += a;
+	}
+
+	cout << "Result: " << result << endl;
 
 	_getch();
 	return 0;
