4f47bc940c09c307bbff000eced9d6867f7ca7ad
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-20 19:55:59 +0200

    Completed Task 3

--- main.cpp
+++ main.cpp
@@ -7,6 +7,29 @@ using std::cin;
 
 int main()
 {
+	long long start;
+	long long end;
+	long long temp;
+	long long result = 0;
+
+	cout << "Enter the start of range: ";
+	cin >> start;
+	cout << "Enter the end of range: ";
+	cin >> end;
+
+	if (start > end) {
+		temp = end;
+		end = start;
+		start = temp;
+	}
+
+	do
+	{
+		result += start;
+		start++;
+	} while (start <= end);
+
+	cout << "Result: " << result << endl;
 
 	_getch();
 	return 0;
