a27a2db46470d9c8831236bc48f2bd69f45be6e1
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-23 12:13:10 +0200

    Completed Task 4

--- main.cpp
+++ main.cpp
@@ -7,7 +7,29 @@ using std::cin;
 
 int main()
 {
+	int number;
+	long long result = 1;
 
+	do
+	{
+		cout << "Enter a number in the range from 1 to 20: ";
+		cin >> number;
+
+		if (number >= 1 && number <= 20)
+		{
+			break;
+		}
+
+		cout << "The number must be in the range from 1 to 20" << endl << endl;
+	} while (true);
+
+	for (; number <= 20; number++)
+	{
+		result *= number;
+	}
+
+	cout << "Result: " << result << endl;
+	
 	_getch();
 	return 0;
 }
