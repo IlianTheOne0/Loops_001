a881390dc5c319162066477abbac69841df25510
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-23 12:19:42 +0200

    Completed Task 5

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ using std::cin;
 
 int main()
 {
+	int number;
+
+	cout << "Enter the number: ";
+	cin >> number;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << number << " * " << i << " = " << (number * i) << endl;
+	}
 
 	_getch();
 	return 0;
